f621d463a2271379e447b754e313db5a1969c7ef
John Yue <john.yxn@gmail.com>
2024-03-21 03:12:17 +0800

    display julia along side mandelbrot

--- main.cpp
+++ main.cpp
@@ -42,6 +42,22 @@ uint32_t mandelbrot(complex_d c)
     return i;
 }
 
+uint32_t julia(complex_d x, complex_d c)
+{
+    double escape_radios = 2.0;
+    uint32_t i = 0;
+    for (; i < MAX_ITERATION; i++)
+    {
+        x = sequence(x, c);
+        if (std::abs(x) > escape_radios)
+        {
+            break;
+        }
+    }
+
+    return i;
+}
+
 void output_image(int **bitmap, int length, int height)
 {
     std::ofstream image;
@@ -69,13 +85,16 @@ public:
     {
         sAppName = "Mandelbrot Display";
 
-        bitmap = new int *[width];
+        bitmapMandelbrot = new int *[width];
+        bitmapJulia = new int *[width];
         for (int i = 0; i < width; i++)
         {
-            bitmap[i] = new int[height];
+            bitmapMandelbrot[i] = new int[height];
+            bitmapJulia[i] = new int[height];
         }
-        gen_image(bitmap, width, height, zoom);
-        Construct(height, width, 1, 1);
+        gen_image_mandelbrot(bitmapMandelbrot, width, height, zoom);
+        gen_image_julia(bitmapJulia, width, height, 0, 0);
+        Construct(width * 2, height, 1, 1);
     }
 
 private:
@@ -88,6 +107,9 @@ private:
     bool OnUserUpdate(float fElapsedTime) override
     {
         double new_zoom = zoom;
+        int32_t mouse_x = GetMouseX();
+        int32_t mouse_y = GetMouseY();
+
         if (GetMouseWheel() > 0)
         {
             std::cout << "mouse up\n";
@@ -104,8 +126,7 @@ private:
         }
         if (new_zoom != zoom)
         {
-            int32_t mouse_x = GetMouseX();
-            int32_t mouse_y = GetMouseY();
+
             fmt::print("mouse position {} {}\n", mouse_x, mouse_y);
             fmt::print("zoom change from {} to {}\n", zoom, new_zoom);
 
@@ -126,21 +147,34 @@ private:
 
             // use the old bitmap to do interpolation
             // while calculating the new bitmap
-            gen_image(bitmap, width, height, new_zoom);
+            gen_image_mandelbrot(bitmapMandelbrot, width, height, new_zoom);
             zoom = new_zoom;
 
             DrawString(30, 30, std::to_string(new_zoom));
+        }
+        if (mouse_x != mouse_x_old || mouse_y != mouse_y_old)
+        {
+            gen_image_julia(bitmapJulia, width, height, mouse_x, mouse_y);
+            // fmt::print("redraw julia");
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    int value = bitmapJulia[x][y];
+                    Draw(x + width, y, olc::Pixel(value, value, value));
+                }
+            }
         }
 
         // called once per frame
         if (should_draw)
         {
             std::cout << "redraw with zoom:" << zoom << "\n";
-            for (int x = 0; x < ScreenWidth(); x++)
+            for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < ScreenHeight(); y++)
+                for (int y = 0; y < height; y++)
                 {
-                    int value = bitmap[x][y];
+                    int value = bitmapMandelbrot[x][y];
                     Draw(x, y, olc::Pixel(value, value, value));
                 }
             }
@@ -150,21 +184,43 @@ private:
         return true;
     }
 
-    void gen_image()
+    void gen_image_julia(int **bitmap, int length, int height, int x, int y)
     {
+        // compute c with regard to mandelbrot set
+        int center_x = length / 2;
+        int center_y = height / 2;
+
+        double step = range / length / zoom;
+        double c_x = (x - center_x) * step + shift_x;
+        double c_y = (y - center_y) * step + shift_y;
+        complex_d c{c_x, c_y};
+
+        // reset step for julia generation
+        step = range / length;
+
+        for (int x = 0; x < length; x++)
+        {
+            double x_d = (x - center_x) * step;
+            for (int y = 0; y < height; y++)
+            {
+                double y_d = (y - center_y) * step;
+                complex_d v{x_d, y_d};
+                uint32_t r = julia(v, c);
+                bitmap[x][y] = r;
+            }
+        }
     }
 
-    void gen_image(int **bitmap, int length, int height, double zoom)
+    void gen_image_mandelbrot(int **bitmap, int length, int height, double zoom)
     {
         total_power_count = 0;
         auto start = rdsysns();
-        std::cout << "gen_image with zoom:" << zoom << " " << length << " " << height << "\n";
 
         int center_x = length / 2;
         int center_y = height / 2;
 
         double step = range / length / zoom;
-        // assuming 2.0 x 2.0 range
+
         for (int x = 0; x < length; x++)
         {
             double x_d = (x - center_x) * step + shift_x;
@@ -181,19 +237,20 @@ private:
 
         should_draw = true;
         auto total_ns = end - start;
-        std::cout << "gen_image completed, elapsed:" << total_ns / 100000
-                  << " total_power:" << total_power_count
-                  << " per power:" << total_ns / total_power_count << "\n ";
+        fmt::print("gen_image_mandelbrot, zoom {}, elapsed {}, total_power {}, ns_per_power {}\n", zoom, total_ns, total_power_count, total_ns / total_power_count);
     }
 
     bool should_draw = true;
-    int **bitmap;
+    int **bitmapMandelbrot;
+    int **bitmapJulia;
     int32_t width;
     int32_t height;
     double zoom = 1.0;
     double range = 3.0;
     double shift_x = -0.8;
     double shift_y = 0.0;
+    int mouse_x_old = 0;
+    int mouse_y_old = 0;
 };
 
 int main()
